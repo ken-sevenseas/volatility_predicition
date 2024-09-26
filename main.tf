e8729847464cb6f07e7030386516460026c8966c
ken-sevenseas <inakanookaki@icloud.com>
2024-09-26 21:03:28 +0900

    Add AWS infrastructure

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  profile = "devken"
+  region  = "ap-southeast-2"
+}
+
+# VPCの作成
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my_vpc"
+  }
+}
+
+# インターネットゲートウェイの作成
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# /24 サブネットの作成
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+# ルートテーブルの作成
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "my_route_table"
+  }
+}
+
+# ルートテーブルをサブネットに関連付け
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
